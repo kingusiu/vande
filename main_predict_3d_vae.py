24c1911f7f7fd5f402e2b4744ca8050476ac14da
kingusiu <3071710+kingusiu@users.noreply.github.com>
2020-08-31 14:54:02 +0200

    adding result path factory to 3d prediction script and removing result-dir setup from experiment

--- main_predict_3d_vae.py
+++ main_predict_3d_vae.py
@@ -1,29 +1,28 @@
 import os
 import setGPU
+import tensorflow as tf
 
 import pofah.util.experiment as ex
 import pofah.util.event_sample as es
 import vae.losses as lo
 from vae.vae_3Dloss_model import VAE_3D
-import pofah.sample_dict as sd
 import config.config as co
 import pofah.util.sample_factory as sf
+import pofah.path_constants.sample_dict_file_parts as sdi 
+import pofah.path_constants.sample_dict_file_parts_reco as sdr 
 
 
 # ********************************************************
 #               runtime params
 # ********************************************************
 
-#test_samples = ['qcdSig', 'GtoWW15na', 'GtoWW15br', 'GtoWW25na', 'GtoWW25br', 'GtoWW45na', 'GtoWW45br']
+test_samples = ['qcdSig', 'GtoWW15na', 'GtoWW15br', 'GtoWW25na', 'GtoWW25br', 'GtoWW45na', 'GtoWW45br']
 #test_samples = ['qcdSide', 'GtoWW35na', 'GtoWW35br']
-test_samples = ['qcdSig']
+#test_samples = ['qcdSig']
 
 run_n = 101
-data_sample = 'particle'
 
-experiment = ex.Experiment(run_n).setup(result_dir=True)
-paths = sf.SamplePathFactory(experiment, data_sample)
-
+experiment = ex.Experiment(run_n=run_n, path_dict=sdi.path_dict)
 
 # ********************************************
 #               load model
@@ -32,44 +31,55 @@ paths = sf.SamplePathFactory(experiment, data_sample)
 vae = VAE_3D(run=run_n, model_dir=experiment.model_dir)
 vae.load( )
 
+input_paths = sf.SamplePathDirFactory(sdi.path_dict)
+result_paths = sf.SamplePathDirFactory(sdr.path_dict).extend_base_path(experiment.run_dir)
+
 for sample_id in test_samples:
 
     # ********************************************
     #               read test data (events)
     # ********************************************
 
-    test_sample = es.EventSample.from_input_file(sample_id, paths.sample_path(sample_id))
-    test_evts_j1, test_evts_j2 = test_sample.get_particles()
 
-    # *******************************************************
-    #               predict test data
-    # *******************************************************
+    list_ds = tf.data.Dataset.list_files(input_paths.sample_path(sample_id)+'/*')
 
-    print('predicting {}'.format(sd.sample_name[sample_id]))
-    test_evts_j1_reco, z_mean_j1, z_log_var_j1 = vae.predict_with_latent(test_evts_j1)
-    test_evts_j2_reco, z_mean_j2, z_log_var_j2 = vae.predict_with_latent(test_evts_j2)
+    for file_path in list_ds:
 
-    # *******************************************************
-    #               compute losses
-    # *******************************************************
+        file_name = file_path.numpy().decode('utf-8').split(os.sep)[-1]
+        test_sample = es.EventSample.from_input_file(sample_id, file_path.numpy().decode('utf-8'))
+        test_evts_j1, test_evts_j2 = test_sample.get_particles()
+        print('{}: {} j1 evts, {} j2 evts'.format(file_path.numpy().decode('utf-8'), len(test_evts_j1), len(test_evts_j2)))
 
-    losses_j1 = lo.compute_loss_of_prediction_3D_kl(test_evts_j1, test_evts_j1_reco, z_mean_j1, z_log_var_j1)
-    losses_j2 = lo.compute_loss_of_prediction_3D_kl(test_evts_j2, test_evts_j2_reco, z_mean_j2, z_log_var_j2)
 
-    # *******************************************************
-    #               add losses to DataSample and save
-    # *******************************************************
+        # *******************************************************
+        #               predict test data
+        # *******************************************************
 
-    reco_sample = es.EventSample(sample_id + 'Reco', particles=[test_evts_j1_reco,test_evts_j2_reco], event_features=test_sample.get_event_features(), particle_feature_names=test_sample.particle_feature_names)
+        print('predicting {}'.format(sdi.path_dict['sample_name'][sample_id]))
+        test_evts_j1_reco, z_mean_j1, z_log_var_j1 = vae.predict_with_latent(test_evts_j1)
+        test_evts_j2_reco, z_mean_j2, z_log_var_j2 = vae.predict_with_latent(test_evts_j2)
 
-    for loss, label in zip( losses_j1, ['j1TotalLoss', 'j1RecoLoss', 'j1KlLoss']):
-        reco_sample.add_event_feature(label, loss)
-    for loss, label in zip( losses_j2, ['j2TotalLoss', 'j2RecoLoss', 'j2KlLoss']):
-        reco_sample.add_event_feature(label, loss)
+        # *******************************************************
+        #               compute losses
+        # *******************************************************
 
-    # *******************************************************
-    #               write predicted data
-    # *******************************************************
+        losses_j1 = lo.compute_loss_of_prediction_3D_kl(test_evts_j1, test_evts_j1_reco, z_mean_j1, z_log_var_j1)
+        losses_j2 = lo.compute_loss_of_prediction_3D_kl(test_evts_j2, test_evts_j2_reco, z_mean_j2, z_log_var_j2)
 
-    print('writing results for {} to {}'.format(sd.sample_name[sample_id],experiment.result_dir))
-    reco_sample.dump(experiment.result_dir)
+        # *******************************************************
+        #               add losses to DataSample and save
+        # *******************************************************
+
+        reco_sample = es.EventSample(sample_id + 'Reco', particles=[test_evts_j1_reco,test_evts_j2_reco], event_features=test_sample.get_event_features(), particle_feature_names=test_sample.particle_feature_names)
+
+        for loss, label in zip( losses_j1, ['j1TotalLoss', 'j1RecoLoss', 'j1KlLoss']):
+            reco_sample.add_event_feature(label, loss)
+            for loss, label in zip( losses_j2, ['j2TotalLoss', 'j2RecoLoss', 'j2KlLoss']):
+                reco_sample.add_event_feature(label, loss)
+
+        # *******************************************************
+        #               write predicted data
+        # *******************************************************
+
+        print('writing results for {} to {}'.format(sdr.path_dict['sample_name'][sample_id], os.path.join(result_paths.sample_path(sample_id), file_name)))
+        reco_sample.dump(os.path.join(result_paths.sample_path(sample_id), file_name))
