2c2eb26c0e515f9b3f6674e522ebc800bd8aba1c
kingusiu <3071710+kingusiu@users.noreply.github.com>
2021-01-19 14:20:26 +0100

    adding minMaxNormalization and -Unnormalization layers

--- vae/layers.py
+++ vae/layers.py
@@ -73,3 +73,30 @@ class StdUnnormalization(StdNormalization):
     def call(self, x):
         return (x * self.std_x) + self.mean_x
 
+
+class MinMaxNormalization(tf.keras.layers.Layer):
+    """normalizing input feature to std Gauss (mean 0, var 1)"""
+    def __init__(self, min_x, max_x, name='MinMax_Normalize', **kwargs):
+        kwargs.update({'name': name, 'trainable': False})
+        super(MinMaxNormalization, self).__init__(**kwargs)
+        self.min_x = min_x
+        self.max_x = max_x
+
+    def get_config(self):
+        config = super(MinMaxNormalization, self).get_config()
+        config.update({'min_x': self.min_x, 'max_x': self.max_x})
+        return config
+
+    def call(self, x):
+        return (x - self.min_x) / (self.max_x - self.min_x)
+
+
+class MinMaxUnnormalization(MinMaxNormalization):
+    """ rescaling feature to original domain """
+
+    def __init__(self, min_x, max_x, name='MinMax_Un_Normalize', **kwargs):
+        super(MinMaxUnnormalization, self).__init__(min_x=min_x, max_x=max_x, name=name, **kwargs)
+
+    def call(self, x):
+        return x * (self.max_x - self.min_x) + self.min_x
+
